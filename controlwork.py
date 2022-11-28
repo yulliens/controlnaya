26fa76ca74651e8002f373bb10e3fff9809dddc5
Юлия Кобыличенко <113581457+yulliens@users.noreply.github.com>
2022-11-28 10:25:37 +0300

    controlwork

---
+++ controlwork.py
@@ -0,0 +1,31 @@
+class Wizard_8:
+    def __init__(self, name, rating, looks_like_age):
+        self.name = name
+        self.rating = rating
+        self.looks_like_age = looks_like_age
+
+
+    def change_rating(self, value):
+        if 1 < self.change_rating + value <= 100:
+            return self.change_rating
+
+
+        elif self.change_rating + value == 47:
+            return self.looks_like_age + abs(value) // 10
+        else:
+            self.looks_like_age < 18
+            return self.looks_like_age == None
+
+        if self.change_rating - 1:
+            return self.looks_like_age + 1
+
+
+string_information = str(Wizard_8)
+print(self.change_rating + len(string_information))
+print(self.looks_like_age - len(string_information) // 10)
+
+
+
+
+
+
